5d97ca3ea02c943da44a9ce285f0053918508106
Muhammad Usman <163582723+musman023@users.noreply.github.com>
2024-03-30 18:03:14 +0500

    change theme

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -21,7 +21,77 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.indigo.shade400,
       ),
       body: Column(
-        children: [],
+        children: [
+          Card(
+            child: ListTile(
+              title: const Text('GetX Dialog Alert'),
+              subtitle: const Text("Showing Dialog Alert Box Using GetX"),
+              onTap: () {
+                Get.defaultDialog(
+                  title: 'Delete Chat',
+                  middleText: 'Are you sure you want to delete chat?',
+                  titlePadding: const EdgeInsets.only(top: 15),
+                  contentPadding: const EdgeInsets.all(18),
+                  confirm: TextButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      child: const Text("Ok")),
+                  cancel: TextButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      child: const Text("Cancel")),
+                );
+              },
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: const Text('GetX Boottom Sheet'),
+              subtitle: const Text("Showing Bottom Sheet Using GetX"),
+              onTap: () {
+                Get.bottomSheet(Container(
+                  decoration: const BoxDecoration(
+                      // color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20))),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        onTap: () {
+                          Get.changeTheme(ThemeData.light());
+                        },
+                        leading: const Icon(Icons.light_mode),
+                        title: const Text("Light Mode"),
+                      ),
+                      ListTile(
+                        onTap: () {
+                          Get.changeTheme(ThemeData.dark());
+                        },
+                        leading: const Icon(Icons.dark_mode),
+                        title: const Text("Dark Mode"),
+                      )
+                    ],
+                  ),
+                ));
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text("Dark Mode"),
+            onTap: () {
+              Get.changeTheme(ThemeData.dark());
+            },
+          ),
+          ListTile(
+            title: const Text("Light Mode"),
+            onTap: () {
+              Get.changeTheme(ThemeData.light());
+            },
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(onPressed: () {
         Get.snackbar(
